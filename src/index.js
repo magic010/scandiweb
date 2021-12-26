42a83f37d92c040a5d5ccc5e9b0c4e052299903e
Mostafa <mostafa.udemy1@gmail.com>
2021-12-26 11:57:13 +0200

    start point

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+      <ApolloProvider client={client}>
+        <App />
+        </ApolloProvider>
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +39,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
